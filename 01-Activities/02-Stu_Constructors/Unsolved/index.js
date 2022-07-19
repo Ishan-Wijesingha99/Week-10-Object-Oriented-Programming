7137287f97b812e90641b4d0f8df92c8edb2ddac
Ishan <ishanwijes@gmail.com>
2022-07-19 19:38:50 +1000

    student activity 2 completed - constructor functions

--- 01-Activities/02-Stu_Constructors/Unsolved/index.js
+++ 01-Activities/02-Stu_Constructors/Unsolved/index.js
@@ -1,7 +1,17 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+const Developer = function(firstName, tech) {
+    this.firstName = firstName;
+    this.tech = tech;
+}
 
 // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+Developer.prototype.introduction = function() {
+    console.log(`Introducing, ${this.firstName}, they love ${this.tech}!`);
+}
 
 // TODO: Create a new object using the 'Developer' constructor
+const ishanObject = new Developer('Ishan', 'JavaScript');
 
 // TODO: Call the 'introduction()' method on the new object
+ishanObject.introduction();
+
