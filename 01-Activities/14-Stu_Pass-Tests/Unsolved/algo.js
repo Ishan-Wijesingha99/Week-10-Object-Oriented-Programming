934955edb87f1d9f206896a2d762e9c50604df8f
Ishan <ishanwijes@gmail.com>
2022-07-21 20:43:55 +1000

    student activity 14 completed - running test for unit tests

--- 01-Activities/14-Stu_Pass-Tests/Unsolved/algo.js
+++ 01-Activities/14-Stu_Pass-Tests/Unsolved/algo.js
@@ -1,15 +1,25 @@
 function Algo() {}
 
 Algo.prototype.reverse = function(str) {
-  // TODO: Your code here
+  return str.split('').reverse().join('')
 };
 
 Algo.prototype.isPalindrome = function(str) {
-  // TODO: Your code here
+  const len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 Algo.prototype.capitalize = function(str) {
-  // TODO: Your code here 
+  return str.split(" ").map(word => {
+    // For each word, we convert the first character to upper case
+    return word.substring(0, 1).toUpperCase() + word.substring(1);
+    // We convert the new array into a string.
+  }).join(" ");
 };
 
 module.exports = Algo;
