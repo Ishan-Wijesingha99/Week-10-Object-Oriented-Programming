5aaf4284c9824e9135ada6ea618a9f5cf220eb1f
Ishan <ishanwijes@gmail.com>
2022-07-21 22:08:43 +1000

    mocking return value activity completed

--- 01-Activities/20-Stu_Mock-Fs/Unsolved/fileIO.js
+++ 01-Activities/20-Stu_Mock-Fs/Unsolved/fileIO.js
@@ -6,8 +6,8 @@ FileIO.prototype.read = function(file) {
   return fs.readFileSync(file, "utf8");
 };
 
-FileIO.prototype.write = function(path, data) {
-  return fs.writeFileSync(path, data);
+FileIO.prototype.write = function(path, content) {
+  return fs.writeFileSync(path, content);
 };
 
 module.exports = FileIO;
