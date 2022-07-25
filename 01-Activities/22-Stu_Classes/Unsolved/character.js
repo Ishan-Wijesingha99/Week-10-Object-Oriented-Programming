dbb1a901053d2e4fc7c87aacd81edc529cd96367
Ishan <ishanwijes@gmail.com>
2022-07-25 19:29:54 +1000

    student activity 22 compled - classes

--- 01-Activities/22-Stu_Classes/Unsolved/character.js
+++ 01-Activities/22-Stu_Classes/Unsolved/character.js
@@ -1,32 +1,51 @@
 class Character {
-  // TODO: Add a constructor
-  // TODO: Create a printStats() method that console logs `this.name`, `this.strength`, and `this.hitPoints`
+  // constructor
+  constructor(name, strength, hitPoints) {
+    this.name = name;
+    this.strength = strength;
+    this.hitPoints = hitPoints;
+  }
 
-  // TODO: Create a isAlive() method that returns a boolean based on whether or not a character's "hitpoints" are <= 0
+  // printStats() method that console logs name, strength and hit points
+  printStats() {
+    console.log(`${this.name} has ${this.strength} for strength and ${this.hitPoints} for hit points`)
+  }
 
-  // TODO: Create a attack() method that accepts an opponent object and decreases the opponent's "hitPoints" by this character's strength
+  // isAlive() method that returns a boolean based on whether or not a character's "hitpoints" are <= 0
+  isAlive() {
+     return this.hitPoints <= 0 ? false : true;
+  }
+
+  // attack() method that accepts an opponent object and decreases the opponent's "hitPoints" by this character's strength
+  attack(opponentObject) {
+    opponentObject.hitPoints = opponentObject.hitPoints - this.strength;
+  }
 }
+
+
 
 // Creates two unique characters using the "character" constructor
 const grace = new Character('Grace', 30, 75);
-const dijkstra = new Character('Dijkstra', 20, 105);
+const jimmy = new Character('Jimmy', 20, 105);
+
+
 
 // This keeps track of whose turn it is
 let graceTurn = true;
 
 grace.printStats();
-dijkstra.printStats();
+jimmy.printStats();
 
 const turnInterval = setInterval(() => {
   // If either character is not alive, end the game
-  if (!grace.isAlive() || !dijkstra.isAlive()) {
+  if (!grace.isAlive() || !jimmy.isAlive()) {
     clearInterval(turnInterval);
     console.log('Game over!');
   } else if (graceTurn) {
-    grace.attack(dijkstra);
-    dijkstra.printStats();
+    grace.attack(jimmy);
+    jimmy.printStats();
   } else {
-    dijkstra.attack(grace);
+    jimmy.attack(grace);
     grace.printStats();
   }
 
